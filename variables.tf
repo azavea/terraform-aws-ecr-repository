b95b3b957f0900aba8010c252ae79a04d5e64ee3
Rocky Breslow <rbreslow@azavea.com>
2020-05-13 10:58:23 -0400

    Upgrade module to support Terraform 0.12 (#6)
    * Upgrade module to support Terraform 0.12
    
    Following the instructions outlined in the Terraform documentation, update the module's configuration to support Terraform 0.12. Despite containing minor changes, this is expected to cause a major version bump, because the changes are not backwards compatible.
    
    * Apply suggestions from code review
    
    Co-authored-by: Cole Kettler <me@colekettler.com>
    
    Co-authored-by: Cole Kettler <me@colekettler.com>

--- variables.tf
+++ variables.tf
@@ -1,9 +1,16 @@
-variable "repository_name" {}
+variable "repository_name" {
+  type        = string
+  description = "Name of the repository"
+}
 
 variable "attach_lifecycle_policy" {
-  default = false
+  default     = false
+  type        = bool
+  description = "If true, an ECR lifecycle policy will be attached"
 }
 
 variable "lifecycle_policy" {
-  default = ""
+  default     = ""
+  type        = string
+  description = "Contents of the ECR lifecycle policy"
 }
